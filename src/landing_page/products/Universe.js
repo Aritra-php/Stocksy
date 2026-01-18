f9f0c68c2b61a3d47900dc9dc90f0aaa852bf96b
Aritra Ghosh <aritra239@gmail.com>
2026-01-18 20:19:45 +0530

    First Commit- landing_page Complated_next_dashboard

---
+++ src/landing_page/products/Universe.js
@@ -0,0 +1,102 @@
+import React from "react";
+
+function Universe() {
+  return (
+    <div className="container mt-5">
+      <div className="row text-center">
+        <h1>The Zerodha Universe</h1>
+        <p>
+          Extend your trading and investment experience even further with our
+          partner platforms
+        </p>
+
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+
+        <hr/>
+
+        <br/>
+        <br/>
+
+         <div className="col-md-4 p-3">
+          <img src="media/images/zerodhaFundhouse.png" className="universe-logo" alt="" />
+          <p className="text-small text-muted mt-2">
+            Our asset management venture
+          </p>
+        </div>
+
+        <div className="col-md-4 p-3">
+          <img src="media/images/sensibullLogo.svg" className="universe-logo" alt="" />
+          <p className="text-small text-muted mt-2">
+            Options trading platform
+          </p>
+        </div>
+
+        <div className="col-md-4 p-3">
+          <img src="media/images/tijori.svg" className="universe-logo" alt="" />
+          <p className="text-small text-muted mt-2">
+            Investment research platform
+          </p>
+        </div>
+
+        <div className="col-md-4 p-3 mt-4">
+          <img src="media/images/streakLogo.png" className="universe-logo" alt="" />
+          <p className="text-small text-muted mt-2">
+            Systematic trading platform
+          </p>
+        </div>
+
+        <div className="col-md-4 p-3 mt-4">
+          <img src="media/images/smallcaseLogo.png" className="universe-logo" alt="" />
+          <p className="text-small text-muted mt-2">
+            Thematic investment platform
+          </p>
+        </div>
+
+        <div className="col-md-4 p-3 mt-4">
+          <img src="media/images/dittoLogo.png" className="universe-logo" alt="" />
+          <p className="text-small text-muted mt-2">
+            Personal advice on life and health insurance
+          </p>
+        </div>
+       
+        <button
+          className="p-2 btn btn-primary fs-5 mb-5"
+          style={{ width: "20%", margin: "0 auto" }}
+        >
+          Signup Now
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Universe;
+
+
+//  <div className="col-4 p-3 mt-5">
+//           <img src="media/images/zerodhaFundhouse.png" />
+//           <p className="text-small text-muted">Our asset management venture</p>
+//         </div>
+//         <div className="col-4 p-3 mt-5">
+//           <img src="media/images/sensibullLogo.svg" />
+//           <p className="text-small text-muted">Opions trading platform</p>
+//         </div>
+//         <div className="col-4 p-3 mt-5">
+//           <img src="media/images/tijori.svg" />
+//           <p className="text-small text-muted">Investment Research Platform</p>
+//         </div>
+//         <div className="col-4 p-3 mt-5">
+//           <img src="media/images/streakLogo.png" />
+//           <p className="text-small text-muted">Systamatic Trading Platform</p>
+//         </div>
+//         <div className="col-4 p-3 mt-5">
+//           <img src="media/images/smallcaseLogo.png" />
+//           <p className="text-small text-muted">Thematic investment platform</p>
+//         </div>
+//         <div className="col-4 p-3 mt-5">
+//           <img src="media/images/dittoLogo.png" />
+//           <p className="text-small text-muted">Personal advice on life and health insurance</p>
+//         </div>
---
+++ src/landing_page/support/CreateTicket.js
@@ -0,0 +1,185 @@
+import React from "react";
+
+function CreateTicket() {
+  return (
+    <div className="container">
+
+      <div className="row p-5 mt-5 mb-5">
+        <h1 className="fs-2">To create a ticket, select a relevant topic</h1>
+
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h4 className="">
+            <i class="fa fa-plus-circle" aria-hidden="true"></i> Account Opening
+          </h4>
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Online Account Opening
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Offline Account Opening
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Company, Partnership and HUF Account
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Opening
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            NRI Account Opening
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Charges at Zerodha
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Zerodha IDFC FIRST Bank 3-in-1 Account
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Getting Started
+          </a>
+          <br />
+        </div>
+
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h4 className="">
+            <i class="fa-solid fa-circle-user" aria-hidden="true"></i> Your Zerodha Account
+          </h4>
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Your Profile
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Account Modification
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            client Master Report & Depository Participant
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Nomination
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Transfer and Conversion of securities 
+          </a>
+          </div>
+
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h4 className="">
+            <i class="fa-solid fa-paper-plane" aria-hidden="true"></i> Kite
+          </h4>
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            IPO
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Traiding FAQ
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Margin Trading Facility and Margins
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Charts & Orders
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Alerts and Nudges 
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+           General
+          </a>
+          <br />
+        </div>
+
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h4 className="">
+            <i class="fa-solid fa-indian-rupee-sign" aria-hidden="true"></i> Funds
+          </h4>
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Add Money
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Withdraw Money
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Add Bank Accounts 
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            E Mandates
+          </a>        
+        </div>
+
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h4 className="">
+            <i class="fa-solid fa-gauge" aria-hidden="true"></i> Console
+          </h4>
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Portfolio
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Corporate Actions
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Fund statements
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Reports
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Profile
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Segement
+            </a>
+          <br />
+        </div>
+
+         <div className="col-4 p-5 mt-2 mb-2">
+          <h4 className="">
+            <i class="fa-solid fa-coins" aria-hidden="true"></i> Coin
+          </h4>
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Mutual Funds
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            National Pension Scheme
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Features on coin
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Payments & Orders
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            General
+          </a>
+          <br />
+        </div>
+
+        </div>
+      </div>
+  );
+}
+
+export default CreateTicket;
